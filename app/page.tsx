eb9f58afbf2bc16c1966300ac7f2e6dee2d11a01
henrymacafee <henry.macafee@gmail.com>
2023-08-16 12:59:11 -0500

    feat: update packages, use better page styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,37 @@
 import dynamic from 'next/dynamic';
 import './globals.css';
 import WindowEffectMidground from './components/WindowEffectMidground';
+import { Box, Typography } from '@mui/material';
 
 const GradientBackground = dynamic(() => import('./components/GradientBackground'), {
     ssr: false
 });
 
+// Extract styles into separate objects
+const containerStyles = {
+    display: 'flex',
+    justifyContent: 'center',
+    height: '95vh'
+};
+
+const typographyStyles = {
+    fontSize: '5rem',
+    color: 'yellow',
+    textShadow: '5px 5px 0px orange, -5px -5px 0px lime',
+    fontWeight: 'bold'
+};
 
 const Home = () => {
     return (
-        <div style={{ cursor: 'pointerl' }}>
-            <GradientBackground>
-                <WindowEffectMidground>
-                    <div style={{ display: 'flex', justifyContent: 'center', height: '95vh' }}>
-                        <div
-                            style={{ fontSize: '5rem', color: 'yellow', textShadow: '5px 5px 0px orange, -5px -5px 0px lime', fontWeight: 'bold' }}
-                        >
-                            hi, my name is Henry
-                        </div>
-                    </div>
-                </WindowEffectMidground>
-            </GradientBackground>
-        </div>
+        <GradientBackground>
+            <WindowEffectMidground>
+                <Box sx={containerStyles}>
+                    <Typography sx={typographyStyles}>
+                        hi, my name is Henry
+                    </Typography>
+                </Box>
+            </WindowEffectMidground>
+        </GradientBackground>
     );
 };
 
