f51d192b39ed4f7be59a5ec9766c02d984eecb7e
henrymacafee <henry.macafee@gmail.com>
2023-08-17 14:59:20 -0500

    fix: arrow key tweaks

--- app/components/ArrowKeys.tsx
+++ app/components/ArrowKeys.tsx
@@ -6,16 +6,18 @@ interface ArrowKeysProps {
 
 const ArrowKeys: FC<ArrowKeysProps> = ({handleKeyDown}) => {
 
-    return <div className="arrow-keys-overlay">
-    <div className="arrows">
-        <div onTouchStart={() => handleKeyDown({ key: 'ArrowUp' } as KeyboardEvent)}>↑</div>
-        <div onTouchStart={() => handleKeyDown({ key: 'ArrowLeft' } as KeyboardEvent)}>←</div>
-        <div onTouchStart={() => handleKeyDown({ key: 'ArrowDown' } as KeyboardEvent)}>↓</div>
-        <div onTouchStart={() => handleKeyDown({ key: 'ArrowRight' } as KeyboardEvent)}>→</div>
-    </div>
-    <div onTouchStart={() => handleKeyDown({ key: 'Enter' } as KeyboardEvent)}>Enter</div>
-    <div onTouchStart={() => handleKeyDown({ key: 'Escape' } as KeyboardEvent)}>Esc</div>
-    </div>
+    return (
+      <div className="arrow-keys-overlay">
+        <div className="arrows">
+            <div onTouchStart={() => handleKeyDown({ key: 'ArrowUp' } as KeyboardEvent)}>↑</div>
+            <div onTouchStart={() => handleKeyDown({ key: 'ArrowLeft' } as KeyboardEvent)}>←</div>
+            <div onTouchStart={() => handleKeyDown({ key: 'ArrowDown' } as KeyboardEvent)}>↓</div>
+            <div onTouchStart={() => handleKeyDown({ key: 'ArrowRight' } as KeyboardEvent)}>→</div>
+            <div onTouchStart={() => handleKeyDown({ key: 'Escape' } as KeyboardEvent)}>Esc</div>
+            <div onTouchStart={() => handleKeyDown({ key: 'Enter' } as KeyboardEvent)}>⏎</div>
+        </div>
+      </div>
+    )
 }
 
-export default ArrowKeys
+export default ArrowKeys;
